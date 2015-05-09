ffb0e49d203c67de89a280101dced268af199e39
memoryboxes <memoryboxes@gmial.com>
2015-05-09 02:35:13 +0000

    init

---
+++ java/helloworld/source/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    private final static int cols = 2, rows = 3;
+    public static void main(String[] argv) {
+        System.out.println("hello, world");
+        int a[][] = new int[rows][cols];
+        System.out.println(a.length);
+    }
+}
+
---
+++ java/package/source/com/headfirstjava/PackageExercise.java
@@ -0,0 +1,9 @@
+package com.headfirstjava;
+
+import javax.swing.*;
+
+public class PackageExercise {
+    public static void main(String[] argv) {
+        System.out.println("hello package");
+    }
+}
