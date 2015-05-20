be1d2c6634fa3a9f5f771a5c8029712badb69176
memoryboxes <memoryboxes@gmail.com>
2015-05-20 09:10:28 +0800

    perfect stack

--- go/src/stacker/stacker.go
+++ go/src/stacker/stacker.go
@@ -10,7 +10,7 @@ func main() {
 	haystack.Push("hay")
 	haystack.Push(-15)
 	haystack.Push([]string{"pin", "clip", "needle"})
-	haystack.Push(81, 52)
+	haystack.Push(81.52)
 	for {
 		item, err := haystack.Pop()
 		if err != nil {
