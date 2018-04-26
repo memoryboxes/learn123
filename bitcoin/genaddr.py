cca876b5cafd70a6cf0d4d93c549edffa971e1b5
memoryboxes <memoryboxes@gmail.com>
2018-04-26 12:14:57 +0800

    add docopt option

--- bitcoin/genaddr.py
+++ bitcoin/genaddr.py
@@ -1,18 +1,36 @@
 # -*- coding: utf-8 -*-
 
+"""doctopt msg send tools
+
+Usage:
+  genaddr.py encode  <private>
+  genaddr.py decode  <addr>
+
+Options:
+  -h --help                                             Show this screen.
+  --version                                             Show version.
+
+Example:
+
+    genaddr.py encode 6bd3b27c591                           # gen address from private 0x6bd3b27c591->1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1
+    genaddr.py decode 1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1    # gen hash160 from address PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1->f92044c7924e58000000000000000000000000000000001e
+
+"""
+
 import codecs
 import hashlib
 import mpmath as mp
 import os
 import sys
 from pycoin import ecdsa, encoding
+from docopt import docopt
 
 def gen_address(private_key):
     #private_key = codecs.encode(os.urandom(32), 'hex').decode()
     secret_exponent= int('0x'+private_key, 0)
     print('WIF: ' + encoding.secret_exponent_to_wif(secret_exponent, compressed=False))
     public_pair = ecdsa.public_pair_for_secret_exponent(ecdsa.secp256k1.generator_secp256k1, secret_exponent)
-    print(public_pair)
+    print('public pair:', public_pair)
     hash160 = encoding.public_pair_to_hash160_sec(public_pair, compressed=True)
     print("hash160: {}".format(hash160.hex()))
     print("Bitcoin address: {}".format(encoding.hash160_sec_to_bitcoin_address(hash160)))
@@ -47,8 +65,15 @@ def decode(addr):
     return (out)
 
 if __name__ == '__main__':
-    key = sys.argv[1]
-    if len(key) < 32:
-        key = '0' * (32 - len(key)) + key
-    print(key)
-    gen_address(key)
+    arguments = docopt(__doc__, version='Pmsg 2.0')
+
+    if arguments['encode']:
+        key = arguments['<private>']
+        if len(key) < 32:
+            key = '0' * (32 - len(key)) + key
+        print('private:',key)
+        gen_address(key)
+    elif arguments['decode']:
+        key = arguments['<addr>']
+        print('address:', key)
+        print(decode(key))
