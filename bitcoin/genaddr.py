8e60bf45cdc789cb39f333813710f4da50559882
memoryboxes <memoryboxes@gmail.com>
2018-05-03 06:38:29 +0800

    add desc for compressed is True

--- bitcoin/genaddr.py
+++ bitcoin/genaddr.py
@@ -15,6 +15,8 @@ Options:
 
 Example:
 
+    WIF Format: compressed
+
     genaddr.py p2addr 6bd3b27c591                                         # gen address from private 0x6bd3b27c591=>1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1
     genaddr.py addr2160 1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1                # gen hash160 from address PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1=>f92044c7924e58000000000000000000000000000000001e
     genaddr.py 1602addr f92044c7924e58000000000000000000000000000000001e  # gen address from hash160 f92044c7924e58000000000000000000000000000000001e=>1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1
@@ -30,7 +32,7 @@ from pycoin import ecdsa, encoding
 from docopt import docopt
 
 
-def gen_address(private_key, compressed=False):
+def gen_address(private_key, compressed=True):
     # private_key = codecs.encode(os.urandom(32), 'hex').decode()
     secret_exponent = int('0x' + private_key, 0)
     print('WIF: ' + encoding.secret_exponent_to_wif(secret_exponent, compressed=compressed))
