1ccabe769e7583118a027524f71dbb7221ab50fa
memoryboxes <memoryboxes@gmail.com>
2018-05-31 10:09:00 +0800

    split by size

--- shell/sort_uniq.sh
+++ shell/sort_uniq.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#./sort_uniq.sh file_to_be_sort file_sorted
+#./sort_uniq.sh file_to_be_sort file_sorted 5000M
 
-lines=$(wc -l $1 | sed 's/ .*//g')
-lines_per_file=`expr $lines / 100`
-split -d -l $lines_per_file $1 __part_$1
+#lines=$(wc -l $1 | sed 's/ .*//g')
+#lines_per_file=`expr $lines / 100`
+#split -d -l $lines_per_file $1 __part_$1
+split -d -b $3 $1 __part_split
 
 for file in __part_*
 do
 {
-  sort $file > sort_$file
+  sort -uo sort_$file $file
+  rm -rf $file
 } &
 done
 wait
