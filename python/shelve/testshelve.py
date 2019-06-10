c20eb67ee2d85fac973c2bb67adb417302f05fa3
memorybox <memoryboxes@gmail.com>
2019-06-10 22:05:28 +0800

    add shelve test

--- python/shelve/testshelve.py
+++ python/shelve/testshelve.py
@@ -3,8 +3,15 @@ import shelve
 import dbm
 from contextlib import closing
 
+import shelvex
+
 if __name__ == '__main__':
-    with closing(shelve.open('/db', writeback=True)) as db:
+    with closing(shelve.open('db', writeback=True)) as db:
         print([key for key in db.keys()])
 
     print(dbm.whichdb('db'))
+
+    with closing(shelvex.open('db2', writeback=True)) as db:
+        print([key for key in db.keys()])
+
+    print(dbm.whichdb('db2'))
